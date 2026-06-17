450e09d862f9f7d5409d808e3574153cfe3d14c3
Muhammad Ali <aliocp3@gmail.com>
2026-06-17 17:29:38 +0500

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import image from "\public\hoston_logo.jpeg";
 // import Navigation from './components/Navigation.tsx';
-// import './about.tsx'
+ import About from './about.tsx'
 
-import Contact from './contact.tsx'
+import Contactus from './contactus.tsx'
 import './App.css'
 
 // import image from '/hoston_logo.png'
@@ -14,10 +14,15 @@ function App() {
   const [count, setCount] = useState(0)
   return (
     <>
+        
+        <div>
+      <Contactus/>
+    </div>
         <div> 
       <h1>Houston Easy Repairs</h1>
        <img src="/hoston_logo.jpeg" width="200" height="100" alt="Description" />; 
     </div>
+    
     
     
     </>
